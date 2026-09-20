5c834b7ca400aa31640fd769f38b02f5b147ebb9
Antony-collika <ronbensimpson@gmail.com>
2026-09-20 23:44:19 +0700

    fix: classify storage failures explicitly

--- app/src/main/java/com/subgrab/app/data/FailureClassifier.kt
+++ app/src/main/java/com/subgrab/app/data/FailureClassifier.kt
@@ -9,6 +9,7 @@ object FailureClassifier {
   httpStatus==403 && exception is HttpFailure -> FailureType.ACCESS_DENIED
   httpStatus==403 -> FailureType.HTTP_403
   httpStatus!=null && httpStatus in 500..599 -> FailureType.SERVER_ERROR
+  exception is StorageFailure -> FailureType.STORAGE_ERROR
   exception is SocketTimeoutException || exception?.javaClass?.simpleName?.contains("Timeout",true)==true -> FailureType.TIMEOUT
   exception is IOException -> FailureType.CONNECTION_ERROR
   exception is org.json.JSONException -> FailureType.PARSE_ERROR
