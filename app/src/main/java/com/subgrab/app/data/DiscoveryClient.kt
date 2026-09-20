f1a865406c343193b9f3977cdadfb6de4c74dbdf
Antony-collika <ronbensimpson@gmail.com>
2026-09-20 19:58:29 +0700

    feat: add API and extractor discovery clients

---
+++ app/src/main/java/com/subgrab/app/data/DiscoveryClient.kt
@@ -0,0 +1,3 @@
+package com.subgrab.app.data
+import com.subgrab.app.domain.VideoItem
+interface DiscoveryClient { suspend fun discoverPlaylist(source:String):List<VideoItem>; suspend fun discoverKeyword(query:String):List<VideoItem> }
